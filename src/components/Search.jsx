95c8c249086b2b655516d8a4af75ab6dd8db58b5
intellerX <anmevel10@gmail.com>
2020-06-25 12:30:35 -0500

    commit 2

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,7 +11,7 @@ const Search = ({isHome}) => {
   return(
     <section className="main">
       <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-      <input type="text" className={inputStyle} placeholder="Buscar..." />
+      <input type="text" className="in" placeholder="Buscar..." />
     </section>
   );
 }
