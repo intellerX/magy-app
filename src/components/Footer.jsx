bbd9d0a21ef707fa4670b574eae11ed26c78ae2a
intellerX <anmevel10@gmail.com>
2020-06-30 11:45:27 -0500

    commitfinal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../assets/styles/components/Footer.scss';
 
 const Footer = () => (
   <footer className="footer">
-    <a href="/">Contáctanos</a>
+    <a href="/Login">Contáctanos</a>
     <a href="/">Información</a>
     <a href="/">Centro de ayuda</a>
   </footer>
